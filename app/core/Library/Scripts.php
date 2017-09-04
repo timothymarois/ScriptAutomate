74527794719c7b70e86d0132e2e72a7cdccb8e5f
Timothy Marois <timothymarois@gmail.com>
2017-09-04 12:15:58 -0400

    Added main parent script class.

---
+++ app/core/Library/Scripts.php
@@ -0,0 +1,18 @@
+<?php namespace App\Library;
+
+use \App\Library\View as View;
+
+class Scripts
+{
+
+    /**
+    * __construct()
+    *
+    *
+    */
+    public function __construct( $parameters = [] )
+	{
+        $this->param = $parameters;
+    }
+
+}
