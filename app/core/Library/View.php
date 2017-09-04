ba208077991e5aa989037b67fde9038ee05a2570
Timothy Marois <timothymarois@gmail.com>
2017-09-04 13:24:13 -0400

    Fixed and added style the script iframe.

--- app/core/Library/View.php
+++ app/core/Library/View.php
@@ -41,7 +41,7 @@ class View
 
         if (!is_cli())
         {
-            echo '<style>body{font-family:"Lucida Console", Monaco, monospace;font-size:12px;color: #1ace22;</style>';
+            echo '<style>body{font-family:"Lucida Console", Monaco, monospace;font-size:12px;color: #1ace22;}a,a:hover,a:active,a:focus{color: #fff;}</style>';
             echo '<script>function toBottom(){window.scrollTo(0,document.body.scrollHeight);}var scrollInterval = setInterval(function(){window.scrollTo(0,document.body.scrollHeight);},20);</script>';
 
             echo '<div style="color:#00A8FF;font-size:11px;"># script started at '.date('Y-m-d H:i:s',self::$timeStart).'</div>';
